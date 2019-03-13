3a073ad36a49316d62d989ae86c2a406ae3b2f0c
Simon Midtvedt <s.midtvedt@gmail.com>
2019-03-13 15:01:36 -0700

    Delete old lecture names

--- Lecture 12/lecture12.R
+++
@@ -1,3 +0,0 @@
-library(bio3d)
-file.name <- get.pdb("1hsg")
-hiv <- read.pdb(file.name)
--- Lecture 7/paletter.R
+++
@@ -1,5 +0,0 @@
-library(paletter)
-
-create_palette(image_path = "~/Google Drive/(1) NTNU/BGGN 213/Lecture 7/Photo on 1-30-19 at 3.02 PM.jpg", number_of_colors = 20, type_of_variable = "categorical")
-
-create_palette(image_path = "~/Google Drive/(1) NTNU/BGGN 213/Lecture 7/Photo on 1-30-19 at 3.07 PM.jpg", number_of_colors = 20, type_of_variable = "categorical")
